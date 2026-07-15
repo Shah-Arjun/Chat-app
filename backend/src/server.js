9a6e5ccf4a7f69ce6243d0b2220b8ec10bbc9a78
Arjun <arjunsah856@gmail.com>
2026-07-15 21:00:16 +0545

    routes defined

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,13 @@ import 'dotenv/config'
 
 //routes
 import authRoutes from './routes/auth.route.js' 
+import messagesRoutes from './routes/messages.route.js' 
 
 const app = express()
 
 
 app.use('/api/auth', authRoutes)
+app.use('/api/messages', messagesRoutes)
 
 
 const PORT = process.env.PORT || 3000
