3da8a92d43dffb991a6519b145bdd501d6f2af52
Arjun <arjunsah856@gmail.com>
2026-07-31 17:59:35 +0545

    code refactor

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -41,6 +41,8 @@ export const signup = async(req, res) => {
                 email: newUser.email,
                 profilePic: newUser.profilePic
             })
+
+            // TODO: Send a welcome email to the user after successful signup
         } else {
             res.status(400).json({ message: "Invalid user data" })
         }
