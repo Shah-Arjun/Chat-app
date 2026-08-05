57b5f450d54ad3c2bcf555584164c960a65f3880
Arjun <arjunsah856@gmail.com>
2026-08-05 20:12:47 +0545

    tailwindCSS v3 and daisyUI v-4.12.24 setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        <button className="btn btn-primary">Button</button>
+    </div>
   )
 }
 
