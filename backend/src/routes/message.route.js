5b0a0a73851a0cedc1429bededd2a68b6220127a
Arjun <arjunsah856@gmail.com>
2026-08-24 20:20:34 +0300

    feat: delete message api

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage } from "../controllers/message.controller.js";
+import { deleteMyMessage, getAllContacts, getChatPartners, getMessagesByUserId, sendMessage } from "../controllers/message.controller.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.middleware.js";
 import { arcjetPotection } from "../middleware/arcjet.middleware.js";
 const router = express.Router()
@@ -11,6 +11,7 @@ router.get('/contacts', getAllContacts)
 router.get('/chats', getChatPartners)
 router.get('/:id', getMessagesByUserId)   // partner's id
 router.post('/send/:id', sendMessage)
+router.delete("/", deleteMyMessage)
 
 
 export default router
