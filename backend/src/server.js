791073d1978ad96d94cc87647a770cf3b1f4926e
Arjun <arjunsah856@gmail.com>
2026-09-11 20:58:56 +0545

    refactors

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,6 @@ import 'dotenv/config'
 import connectDB from './lib/db.js'
 import cookieParser from 'cookie-parser'
 
-//routes
 import authRoutes from './routes/auth.route.js' 
 import messagesRoutes from './routes/message.route.js' 
 import { ENV } from './lib/env.js'
@@ -16,8 +15,10 @@ app.use(cors({ origin: ENV.CLIENT_URL, credentials: true}))
 app.use(cookieParser())   // to parse cookies from the request
 
 
+// endpoints
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messagesRoutes)
+
 
 
 const PORT = ENV.PORT || 3000
